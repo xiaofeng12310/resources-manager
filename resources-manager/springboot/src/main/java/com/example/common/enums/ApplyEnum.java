3303b17c01a21b743518cf4b8c9d5d33ceba7c14
xiaofeng12310 <486095908@qq.com>
2025-03-31 09:49:10 +0800

    Initial commit

---
+++ resources-manager/springboot/src/main/java/com/example/common/enums/ApplyEnum.java
@@ -0,0 +1,27 @@
+package com.example.common.enums;
+
+public enum ApplyEnum {
+    // 两个进度
+    PROCESS_HEADER_APPLYING("主管审批中"),
+    PROCESS_ADMIN_APPLYING("管理员审批中"),
+
+    // 每个进度三种审批状态
+    STATUS_HEADER_APPLYING("待主管审批"),
+    STATUS_HEADER_APPLY_OK("主管审批通过"),
+    STATUS_HEADER_APPLY_NO("主管审批不通过"),
+
+    STATUS_ADMIN_APPLYING("待管理员审批"),
+    STATUS_ADMIN_APPLY_OK("管理员审批通过"),
+    STATUS_ADMIN_APPLY_NO("管理员审批不通过"),
+
+    APPLY_OK("审批通过"),
+    APPLY_NO("审批不通过"),
+    APPLY_DONE("审批结束"),
+    ;
+
+    public String status;
+
+    ApplyEnum(String status) {
+        this.status = status;
+    }
+}
---
+++ resources-manager/springboot/src/main/java/com/example/mapper/SalaryMapper.java
@@ -0,0 +1,40 @@
+package com.example.mapper;
+
+import com.example.entity.Salary;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+/**
+ * 操作salary相关数据接口
+*/
+public interface SalaryMapper {
+
+    /**
+      * 新增
+    */
+    int insert(Salary salary);
+
+    /**
+      * 删除
+    */
+    int deleteById(Integer id);
+
+    /**
+      * 修改
+    */
+    int updateById(Salary salary);
+
+    /**
+      * 根据ID查询
+    */
+    Salary selectById(Integer id);
+
+    /**
+      * 查询所有
+    */
+    List<Salary> selectAll(Salary salary);
+
+    @Select("select distinct year from salary group by year")
+    List<Salary> getMonth();
+}
